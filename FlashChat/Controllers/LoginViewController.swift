f73a0717c332ca05110a5ecc02bdd6cee8244dda
SerhiiPavlichuk <S_Pavlichuk@icloud.com>
2022-04-19 21:50:36 +0300

    login auth finished

--- FlashChat/Controllers/LoginViewController.swift
+++ FlashChat/Controllers/LoginViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
 
@@ -19,6 +20,14 @@ class LoginViewController: UIViewController {
     }
 
     @IBAction func loginButtonPressed(_ sender: UIButton) {
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                if let error = error {
+                    print(error)
+                } else {
+                    self.performSegue(withIdentifier: "LoginToChat", sender: self)
+                }
+            }
+        }
     }
-    
 }
